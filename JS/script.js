fb977b1c40e276cfecd8d2d03330ed2f91a97305
Oleksandr Shyshkov <alexsedition@gmail.com>
2021-02-26 11:52:47 +0200

    task1

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+      
+
+    let numberOfFilms = prompt('how many films have you already watched?' , '' );
+    if ( numberOfFIlms < '10') {
+           console.log('you are beginner watcher');
+       }
+
+    else if (numberOfFilms > '20') {
+           console.log('you watch a lot');
+       }
+
+    else
+           {
+               console.log('you are middle watcher');
+       }
+
+       
+
+
